613a962fba4e8d88e61950f124ac0ce89efc7b7f
lakshay8610 <manjeetrao.sir@gmail.com>
2025-12-05 13:30:30 +0530

    Update fetch URL from questions.json to question.json
    Fix incorrect fetch URL for questions data.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function startGame(level) {
     document.getElementById("level-screen").classList.add("hidden");
     document.getElementById("quiz-screen").classList.remove("hidden");
 
-fetch("questions.json?v=" + Date.now())
+fetch("question.json?v=" + Date.now())
     let data = await res.json();
 
     questions = data[level];  // load selected level questions
